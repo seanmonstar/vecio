14280eba758ab7e89a48678002538c9eb0d384f6
Sean McArthur <sean.monstar@gmail.com>
2015-11-30 14:46:57 -0800

    try different addr for windows test

--- src/sys/windows.rs
+++ src/sys/windows.rs
@@ -72,7 +72,7 @@ fn test_rawv() {
     use std::net::{TcpListener, TcpStream};
     use std::thread;
 
-    let acceptor = TcpListener::bind("0.0.0.0:0").unwrap();
+    let acceptor = TcpListener::bind("127.0.0.1:3444").unwrap();
     let addr = acceptor.local_addr().unwrap();
 
     thread::spawn(move || {
